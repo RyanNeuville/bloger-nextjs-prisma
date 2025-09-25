fa8f4443ad3fe0fea64aad01a3405116a0e90db3
Ryan Neuville <neuvilleryan123@gmail.com>
2025-09-25 05:30:32 +0100

    Allow the user to log in through the Header.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,33 +1,34 @@
 import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { signOut, useSession } from "next-auth/react";
 
 const Header: React.FC = () => {
   const router = useRouter();
   const isActive: (pathname: string) => boolean = (pathname) =>
     router.pathname === pathname;
-
+  const { data: session, status } = useSession();
   let left = (
     <div className="left">
       <Link href="/" className="bold" data-active={isActive("/")}>
-        Feed
+        <span>Feed</span>
       </Link>
       <style jsx>{`
         .bold {
           font-weight: bold;
         }
 
-        a {
+        span {
           text-decoration: none;
           color: #000;
           display: inline-block;
         }
 
-        .left a[data-active="true"] {
+        .left span[data-active="true"] {
           color: gray;
         }
 
-        a + a {
+        span + span {
           margin-left: 1rem;
         }
       `}</style>
@@ -35,6 +36,157 @@ const Header: React.FC = () => {
   );
 
   let right = null;
+
+  if (status === "loading") {
+    left = (
+      <div className="left">
+        <Link href="/" className="bold" data-active={isActive("/")}>
+          <span>Feed</span>
+        </Link>
+        <style jsx>{`
+          .bold {
+            font-weight: bold;
+          }
+
+          span {
+            text-decoration: none;
+            color: var(--geist-foreground);
+            display: inline-block;
+          }
+
+          .left span[data-active="true"] {
+            color: gray;
+          }
+
+          span + span {
+            margin-left: 1rem;
+          }
+        `}</style>
+      </div>
+    );
+    right = (
+      <div className="right">
+        <p>Validating session ...</p>
+        <style jsx>{`
+          .right {
+            margin-left: auto;
+          }
+        `}</style>
+      </div>
+    );
+  }
+
+  if (!session) {
+    right = (
+      <div className="right">
+        <Link href="/api/auth/signin" data-active={isActive("/signup")}>
+          <span>Log in</span>
+        </Link>
+        <style jsx>{`
+          span {
+            text-decoration: none;
+            color: var(--geist-foreground);
+            display: inline-block;
+          }
+
+          span + span {
+            margin-left: 1rem;
+          }
+
+          .right {
+            margin-left: auto;
+          }
+
+          .right span {
+            border: 1px solid var(--geist-foreground);
+            padding: 0.5rem 1rem;
+            border-radius: 3px;
+          }
+        `}</style>
+      </div>
+    );
+  }
+
+  if (session) {
+    left = (
+      <div className="left">
+        <Link href="/">
+          <span className="bold" data-active={isActive("/")}>
+            Feed
+          </span>
+        </Link>
+        <Link href="/drafts">
+          <span data-active={isActive("/drafts")}>My drafts</span>
+        </Link>
+        <style jsx>{`
+          .bold {
+            font-weight: bold;
+          }
+
+          span {
+            text-decoration: none;
+            color: var(--geist-foreground);
+            display: inline-block;
+          }
+
+          .left span[data-active="true"] {
+            color: gray;
+          }
+
+          span + span {
+            margin-left: 1rem;
+          }
+        `}</style>
+      </div>
+    );
+
+    right = (
+      <div className="right">
+        <p>
+          {session.user.name} ({session.user.email})
+        </p>
+        <Link href="/create">
+          <button>
+            <span>New post</span>
+          </button>
+        </Link>
+        <button onClick={() => signOut()}>
+          <span>Log out</span>
+        </button>
+        <style jsx>{`
+          span {
+            text-decoration: none;
+            color: var(--geist-foreground);
+            display: inline-block;
+          }
+
+          p {
+            display: inline-block;
+            font-size: 13px;
+            padding-right: 1rem;
+          }
+
+          span + span {
+            margin-left: 1rem;
+          }
+
+          .right {
+            margin-left: auto;
+          }
+
+          .right span {
+            border: 1px solid var(--geist-foreground);
+            padding: 0.5rem 1rem;
+            border-radius: 3px;
+          }
+
+          button {
+            border: none;
+          }
+        `}</style>
+      </div>
+    );
+  }
 
   return (
     <nav>
